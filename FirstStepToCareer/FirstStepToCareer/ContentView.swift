17bb291b45a42ebe4912ee877dc90ac5c7f219ff
znak99 <xhahekcldhkznak0113@gmail.com>
2025-08-16 22:02:02 +0900

    앱 초기화하고 앱아이콘 추가

---
+++ FirstStepToCareer/FirstStepToCareer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstStepToCareer
+//
+//  Created by seungwoo on 2025/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstStepToCareer/FirstStepToCareer/FirstStepToCareerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstStepToCareerApp.swift
+//  FirstStepToCareer
+//
+//  Created by seungwoo on 2025/08/16.
+//
+
+import SwiftUI
+
+@main
+struct FirstStepToCareerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
