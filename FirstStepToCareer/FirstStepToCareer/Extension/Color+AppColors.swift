d9b00620636eb04cec85e2b4805d922ae958dcbe
znak99 <xhahekcldhkznak0113@gmail.com>
2025-08-18 00:58:11 +0900

    .gitignore 추가

---
+++ FirstStepToCareer/FirstStepToCareer/Extension/Color+AppColors.swift
@@ -0,0 +1,40 @@
+//
+//  Color+AppColors.swift
+//  FirstStepToCareer
+//
+//  Created by seungwoo on 2025/08/16.
+//
+
+import SwiftUI
+
+extension Color {
+    // 색깔을 HEX로 초기화하는 로직
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let red = Double((rgb >> 16) & 0xFF) / 255.0
+        let green = Double((rgb >> 8) & 0xFF) / 255.0
+        let blue = Double(rgb & 0xFF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
+    }
+    
+    // App
+    static let appPrimaryGradient01 = Color(hex: "#1EA9FF")
+    static let appPrimaryGradient02 = Color(hex: "#1670A8")
+    static let appBackground = Color(hex: "#F2F2F2")
+    static let appAccentColor = Color(hex: "#1D9BE9")
+    static let appGrayFont = Color(hex: "#959595")
+    
+    // Tab bar
+    static let appTabBarBackground = Color(hex: "#FEFEFE")
+    static let appTabBarAccent = Color(hex: "#1A8ED6")
+    static let appTabBarGray = Color(hex: "#BFBFBF")
+    
+    // Interview
+    static let appInterviewGradient = Color(hex: "#B8E4FF")
+}
---
+++ FirstStepToCareer/FirstStepToCareer/Model/Global/AppPage.swift
@@ -0,0 +1,31 @@
+//
+//  AppPage.swift
+//  FirstStepToCareer
+//
+//  Created by seungwoo on 2025/08/16.
+//
+
+import Foundation
+
+// 네비게이션을 제어용 각 뷰들 정의
+// TODO: - 앱 구성 다 되면 화면들 추가
+enum AppPage {
+    // Interview
+    case interviewInfoCompanyTypeListView
+    case interviewInfoCareerTypeListView
+    case mockInterviewPrepareView
+    case mockInterviewView
+    case mockInterviewResultView
+    
+    // List
+    
+    // Schedule
+    
+    // Analysis
+    
+    // Settings
+    
+    // Auth
+    case loginView
+    case registrationView
+}
