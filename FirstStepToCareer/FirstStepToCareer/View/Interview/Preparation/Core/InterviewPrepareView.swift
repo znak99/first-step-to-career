0a27091e0e1ea314716f449be8f929ca0f6be6d4
znak99 <xhahekcldhkznak0113@gmail.com>
2025-08-22 01:08:20 +0900

    InterviewPrepareView 중간까지 작성

--- FirstStepToCareer/FirstStepToCareer/View/Interview/Preparation/Core/InterviewPrepareView.swift
+++ FirstStepToCareer/FirstStepToCareer/View/Interview/Preparation/Core/InterviewPrepareView.swift
@@ -9,8 +9,64 @@ import SwiftUI
 
 struct InterviewPrepareView: View {
     @FocusState private var focus: FocusTarget?
+    @EnvironmentObject private var nc: NavigationController
+    @State var text = ""
     var body: some View {
-        Text("InterviewPrepareView")
+        ZStack {
+            Color.appBackground.ignoresSafeArea()
+            
+            VStack {
+                AppSection {
+                    AppSectionHeader(icon: InterviewTabIcon.header, text: "会社名")
+                    CustomTextField(
+                        placeholder: "株式会社就活一歩",
+                        text: $text,
+                        focus: $focus,
+                        focusTarget: .companyName)
+                    AppSectionHeader(icon: InterviewTabIcon.header, text: "選考区分")
+                    CustomTextField(
+                        placeholder: "株式会社就活一歩",
+                        text: $text,
+                        focus: $focus,
+                        focusTarget: .companyName)
+                    AppSectionHeader(icon: InterviewTabIcon.header, text: "企業区分")
+                    CustomTextField(
+                        placeholder: "株式会社就活一歩",
+                        text: $text,
+                        focus: $focus,
+                        focusTarget: .companyName)
+                    AppSectionHeader(icon: InterviewTabIcon.header, text: "希望職種")
+                    CustomTextField(
+                        placeholder: "株式会社就活一歩",
+                        text: $text,
+                        focus: $focus,
+                        focusTarget: .companyName)
+                }
+                AppSection {
+                    GradientNavigationButton(title: "この内容で進める", icon: InterviewTabIcon.Interview.focus) {
+                        nc.pagePath.append(.interviewView)
+                    }
+                }
+                Spacer()
+            }
+            .padding(.horizontal, 16)
+        }
+        .navigationBarBackButtonHidden(true)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button(
+                    action: {
+                        
+                    },
+                    label: {
+                        
+                    }
+                )
+            }
+        }
+        .onDisappear {
+            nc.pagePath.removeLast()
+        }
     }
 }
 
