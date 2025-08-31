fb47b5e249fdcb5a4f7fb94b8cecce8a7fe25e25
znak99 <xhahekcldhkznak0113@gmail.com>
2025-08-31 23:02:01 +0900

    InterviewUsecase 프로토콜에 익스텐션으로 패턴추가

--- FirstStepToCareer/FirstStepToCareer/Domain/Protocol/InterviewUsecase.swift
+++ FirstStepToCareer/FirstStepToCareer/Domain/Protocol/InterviewUsecase.swift
@@ -13,3 +13,21 @@ protocol InterviewUsecase {
     
     func execute(_ req: Req) async throws -> Res
 }
+
+extension InterviewUsecase where Req == Void {
+    func execute() async throws -> Res {
+        try await execute(())
+    }
+}
+
+extension InterviewUsecase where Res == Void {
+    func execute(_ req: Req) async throws {
+        try await execute(req)
+    }
+}
+
+extension InterviewUsecase where Req == Void, Res == Void {
+    func execute() async throws {
+        try await execute(())
+    }
+}
